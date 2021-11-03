82bea838cfec2ee7beee6dac3a6f621ab9f77fed
Oula Kivalo <oula.kivalo@aalto.fi>
2021-11-03 17:41:49 +0200

    deploy

---
+++ assets/ConversationEditor.7e2de88b.js
@@ -0,0 +1,1 @@
+import{l as o,o as e,c as t}from"./vendor.5e3fbbc5.js";import{s as r}from"./transform.f235dda1.js";import{_ as s}from"./index.69b40187.js";const _={id:"editor"},n={setup(a){return o(function(){console.log(r("#editor"))}),(c,d)=>(e(),t("svg",_))}};var l=s(n,[["__scopeId","data-v-53295600"]]);export{l as default};
